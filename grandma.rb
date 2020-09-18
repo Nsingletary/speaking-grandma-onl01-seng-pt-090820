8cb7cfb01ac44b8cbe1fa20f76510a47e165d731
candescent-tuple-8449 <singletarybush@gmail.com>
2020-09-18 05:32:46 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
-def speak_to_grandma(saying)
-  if saying.upcase != saying
+def speak_to_grandma(speaking)
+  if speaking.upcase != speaking
     return "HUH?! SPEAK UP, SONNY!"
-  elsif saying == "I LOVE YOU GRANDMA!"
+  elsif speaking == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   else
     return "NO, NOT SINCE 1938!"
